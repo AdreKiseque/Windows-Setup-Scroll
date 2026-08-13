1a1e3ecbff1098ba3e9a6102599923e471caef57
André de Oliveira <andrefdeo@gmail.com>
2026-08-12 20:10:32 -0700

    Add standard US keyboard for like coding and stuff

--- Setup1.ps1
+++ Setup1.ps1
@@ -87,6 +87,7 @@ $LangJob = Install-Language en-CA -AsJob
 
 $LanguageList = New-WinUserLanguageList en-CA
 $LanguageList[0].InputMethodTips[0] = '1009:00020409' # US-International keyboard
+$LanguageList[0].InputMethodTips.Add('1009:00000409')
 Set-WinUserLanguageList $LanguageList -Force
 
 Set-WinHomeLocation -GeoId 0x27
